4f4fd506cee5563cd74888dc29f18f822654ad22
Luis Mastrangelo <luismastrangelo@gmail.com>
2025-03-31 05:42:32 +0200

    Improve unit tests

--- test/mdpp.test.js
+++ test/mdpp.test.js
@@ -1,35 +1,16 @@
 import { expect } from 'chai';
 import { mdpp } from 'mdpp';
+import { readFiles, testCases } from './.test-cases.js';
 
-const opts = {
-    /**
-     * @param {string} cmd 
-     * @returns {string}
-     */
-    exec: cmd => `cmd is ${cmd}`,
+describe('::mdpp', function () {
 
-    /**
-     * @param {string} path 
-     * @returns {string}
-     */
-    readFile: path => `1234 ${path}`,
-};
-
-describe('asdf', function () {
-
-    [
-        { title: 'empty', input: '', output: '\n' },
-        { title: 'sdi', input: '\n', output: '\n' },
-        { title: 'sdi', input: '\n\n\n  ', output: '\n' },
-        {
-            title: 'sdi', input: `
-\`\`\`js path/to/file
-\`\`\`
-            `, output: '\n'
-        },
-    ].forEach(({ title, input, output }) => {
-        it(title, function () {
-            expect(mdpp(input, opts)).to.be.equal(output);
+    testCases.forEach(({ name, input, output }) => {
+        it(`should preprocess \`${name}\``, function () {
+            expect(mdpp(input, {
+                exec: cmd => `cmd is ${cmd}`,
+                readFile: path => readFiles[path],
+            })).to.be.equal(output);
         });
-    })
+    });
+
 });

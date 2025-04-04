99b270daee3650b960f156fa7a2e31984723c6af
Luis Mastrangelo <luismastrangelo@gmail.com>
2025-04-05 01:54:40 +0200

    Inline test cases

--- test/mdpp.test.js
+++ test/mdpp.test.js
@@ -1,15 +1,51 @@
 import { expect } from 'chai';
 import { mdpp } from 'mdpp';
-import { readFiles, testCases } from './.test-cases.js';
+import { EventEmitter } from 'events';
 
 describe('::mdpp', function () {
 
-    testCases.forEach(({ name, input, output }) => {
+    /** @type {{[file: string]: string}} */
+    const readFiles = {
+        'path/to/empty': '',
+        'path/to/file1': 'single line',
+    };
+
+    [
+        { name: 'empty', input: '', output: '\n', expectedEvents: [] },
+        { name: 'no new-line', input: 'first line', output: 'first line\n', expectedEvents: [] },
+        { name: 'empty line', input: '\n', output: '\n', expectedEvents: [] },
+        { name: 'single line', input: 'first line\n', output: 'first line\n', expectedEvents: [] },
+        { name: 'only whitespaces', input: '\n\n\n  ', output: '\n', expectedEvents: [] },
+        {
+            name: 'empty file',
+            input: `# Headline
+\`\`\`js path/to/empty
+\`\`\`
+            `,
+            output: `# Headline
+\`\`\`js path/to/empty
+
+\`\`\`
+`, expectedEvents: [
+                'path/to/empty',
+                'path/to/empty',
+            ]
+        },
+    ].forEach(({ name, input, output, expectedEvents }) => {
         it(`should preprocess \`${name}\``, function () {
+            /** @type{string[]} */
+            const events = [];
+
             expect(mdpp(input, {
                 exec: cmd => `cmd is ${cmd}`,
                 readFile: path => readFiles[path],
+                eventEmitter: /** @type {import('mdpp').MdppEventEmitter} */ (new EventEmitter())
+                    .on('marker', file => events.push(file))
+                    .on('exec', file => events.push(file))
+                    .on('verbatim', file => events.push(file))
             })).to.be.equal(output);
+
+            expect(events).to.be.deep.equal(expectedEvents);
         });
     });
 
